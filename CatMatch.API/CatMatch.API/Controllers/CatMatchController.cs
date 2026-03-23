4d069ebb13427d0df789fa046e2ef4dd5e99cb15
stevenzab <steven.zabandith@epitech.eu>
2026-03-23 15:30:02 +0100

    [ADD] CatMatchController and DI abstractions dependency
    Added initial CatMatchController with route attributes. Updated project to include Microsoft.Extensions.DependencyInjection.Abstractions for CatMatch.Application.

---
+++ CatMatch.API/CatMatch.API/Controllers/CatMatchController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CatMatch.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CatMatchController : ControllerBase
+    {
+
+    }
+}
