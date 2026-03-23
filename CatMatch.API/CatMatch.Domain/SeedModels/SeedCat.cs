2a17a2a600c263a6af7f2a1c3db229eacc4ba2ea
stevenzab <steven.zabandith@epitech.eu>
2026-03-23 14:41:55 +0100

    Add MongoDB.Bson and domain models for MongoDB support
    Added MongoDB.Bson NuGet package to enable BSON serialization. Introduced RepositoryCollection base class, Cat model, SeedCat seeding model, and MongoSettings for MongoDB configuration. Updated project assets and assembly info accordingly.

---
+++ CatMatch.API/CatMatch.Domain/SeedModels/SeedCat.cs
@@ -0,0 +1,14 @@
+﻿using CatMatch.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatMatch.Domain.SeedModels
+{
+    public class SeedCat
+    {
+        public List<Cat> Images { get; set; }
+    }
+}
