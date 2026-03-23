5704eeae076dc1038f38ac1db30a3c5cf2731dd9
stevenzab <steven.zabandith@epitech.eu>
2026-03-23 15:27:28 +0100

    [ADD] CatDto class with Url and Vote properties
    Introduced CatDto.cs in the CatMatch.Domain.Dto namespace. The class includes Url and Vote properties to represent cat data, with standard .NET using directives.

---
+++ CatMatch.API/CatMatch.Domain/Dto/CatDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatMatch.Domain.Dto
+{
+    public class CatDto
+    {
+        public string Url { get; set; }
+
+        public int Vote { get; set; }
+    }
+}
