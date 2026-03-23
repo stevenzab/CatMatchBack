4d47a3779532c4353c25281c5ee01178bf164005
stevenzab <steven.zabandith@epitech.eu>
2026-03-23 16:16:31 +0100

    [ADD] dataAccess method to retrieve all cats and add a vote in DB

---
+++ CatMatch.API/CatMatch.Application/Services/CatMatch/CatMatchDataAccess.cs
@@ -0,0 +1,32 @@
+﻿using CatMatch.Domain.Dto;
+using CatMatch.Domain.Models;
+using CatMatch.Infrastructure.Common;
+using MongoDB.Driver.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatMatch.Application.Services.CatMatch
+{
+    public class CatMatchDataAccess : ICatMatchDataAccess
+    {
+        private readonly IBaseRepository baseRepository;
+
+        public CatMatchDataAccess(IBaseRepository baseRepository)
+        {
+            this.baseRepository = baseRepository;
+        }
+
+        public async Task<IEnumerable<Cat>> GetAllCatAsync()
+        {
+            return await baseRepository.AsQueryable<Cat>().OrderByDescending(x => x.Vote).ToListAsync();
+        }
+
+        public async Task VoteCat(Cat cat)
+        {
+            await baseRepository.AddAsync(cat);
+        }
+    }
+}
---
+++ CatMatch.API/CatMatch.Application/Services/CatMatch/ICatMatchDataAccess.cs
@@ -0,0 +1,16 @@
+﻿using CatMatch.Domain.Dto;
+using CatMatch.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatMatch.Application.Services.CatMatch
+{
+    public interface ICatMatchDataAccess
+    {
+        Task<IEnumerable<Cat>> GetAllCatAsync();
+        Task VoteCat(Cat cat);
+    }
+}
