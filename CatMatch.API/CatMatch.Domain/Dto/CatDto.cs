a697d4f4279064d67f7f797bdb0dc944854c1cf8
stevenzab <steven.zabandith@epitech.eu>
2026-03-23 16:15:44 +0100

    [ADD] ID field DTO

--- CatMatch.API/CatMatch.Domain/Dto/CatDto.cs
+++ CatMatch.API/CatMatch.Domain/Dto/CatDto.cs
@@ -8,6 +8,8 @@ namespace CatMatch.Domain.Dto
 {
     public class CatDto
     {
+        public string Id { get; set; }
+
         public string Url { get; set; }
 
         public int Vote { get; set; }
