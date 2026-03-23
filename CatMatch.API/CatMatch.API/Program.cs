498e48c91e861b92ef49dc4724be7c9e47712793
stevenzab <steven.zabandith@epitech.eu>
2026-03-23 14:44:24 +0100

    Add infrastructure layer and dependencies to API project
    Added project references to Application and Infrastructure layers in CatMatch.API. Integrated AddInfrastructure for DI setup in Program.cs. Included NuGet packages for configuration and MongoDB support. Updated cache/assets files and added response.json with sample cat images. Updated assembly info to reflect changes.

--- CatMatch.API/CatMatch.API/Program.cs
+++ CatMatch.API/CatMatch.API/Program.cs
@@ -1,3 +1,5 @@
+using CatMatch.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +7,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services
+    .AddSwaggerGen()
+    .AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
