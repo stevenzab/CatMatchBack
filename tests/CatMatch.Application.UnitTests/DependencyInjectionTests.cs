b961719a16095c7991044a6fba548fd746a0ee0d
stevenzab <steven.zabandith@epitech.eu>
2026-03-26 10:31:42 +0100

    [ADD] dependencyInjection unit tests

---
+++ tests/CatMatch.Application.UnitTests/DependencyInjectionTests.cs
@@ -0,0 +1,96 @@
+﻿using CatMatch.Application;
+using CatMatch.Application.Services;
+using CatMatch.Application.Services.CatMatch;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CatMatch.Application.UnitTests
+{
+    [TestClass]
+    public class DependencyInjectionTests
+    {
+
+        [TestMethod]
+        public void AddApplication_RegistersCatMatchServiceAsScoped()
+        {
+            // Arrange
+            var services = new ServiceCollection();
+
+            // Act
+            services.AddApplication();
+
+            // Assert
+            var descriptor = services.FirstOrDefault(sd => sd.ServiceType == typeof(ICatMatchService));
+            Assert.IsNotNull(descriptor);
+            Assert.AreEqual(ServiceLifetime.Scoped, descriptor.Lifetime);
+            Assert.AreEqual(typeof(CatMatchService), descriptor.ImplementationType);
+        }
+
+        [TestMethod]
+        public void AddApplication_RegistersCatMatchDataAccessAsScoped()
+        {
+            // Arrange
+            var services = new ServiceCollection();
+
+            // Act
+            services.AddApplication();
+
+            // Assert
+            var descriptor = services.FirstOrDefault(sd => sd.ServiceType == typeof(ICatMatchDataAccess));
+            Assert.IsNotNull(descriptor);
+            Assert.AreEqual(ServiceLifetime.Scoped, descriptor.Lifetime);
+            Assert.AreEqual(typeof(CatMatchDataAccess), descriptor.ImplementationType);
+        }
+
+        [TestMethod]
+        public void AddApplication_ReturnsIServiceCollection()
+        {
+            // Arrange
+            var services = new ServiceCollection();
+
+            // Act
+            var result = services.AddApplication();
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreSame(services, result);
+        }
+
+        [TestMethod]
+        public void AddApplication_RegistersBothServices()
+        {
+            // Arrange
+            var services = new ServiceCollection();
+
+            // Act
+            services.AddApplication();
+
+            // Assert
+            var descriptors = services.ToList();
+            Assert.AreEqual(2, descriptors.Count);
+
+            var serviceTypes = descriptors.Select(d => d.ServiceType).ToList();
+            Assert.IsTrue(serviceTypes.Contains(typeof(ICatMatchService)));
+            Assert.IsTrue(serviceTypes.Contains(typeof(ICatMatchDataAccess)));
+        }
+
+        [TestMethod]
+        public void AddApplication_RegistersCorrectImplementations()
+        {
+            // Arrange
+            var services = new ServiceCollection();
+
+            // Act
+            services.AddApplication();
+
+            // Assert
+            var serviceDescriptor = services.FirstOrDefault(sd => sd.ServiceType == typeof(ICatMatchService));
+            Assert.IsNotNull(serviceDescriptor);
+            Assert.AreEqual(typeof(CatMatchService), serviceDescriptor.ImplementationType);
+
+            var dataAccessDescriptor = services.FirstOrDefault(sd => sd.ServiceType == typeof(ICatMatchDataAccess));
+            Assert.IsNotNull(dataAccessDescriptor);
+            Assert.AreEqual(typeof(CatMatchDataAccess), dataAccessDescriptor.ImplementationType);
+        }
+    }
+}
--- tests/CatMatch.Application.UnitTests/obj/Debug/net8.0/CatMatch.Application.UnitTests.AssemblyInfo.cs
+++ tests/CatMatch.Application.UnitTests/obj/Debug/net8.0/CatMatch.Application.UnitTests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CatMatch.Application.UnitTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5be4548085441d96512e57e38a7f40c8f276e3a5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b412ac537c85c87cfde0cebc37439919d1fb8c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("CatMatch.Application.UnitTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CatMatch.Application.UnitTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
