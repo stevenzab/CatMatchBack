93f6f3373170d464fc60db13aee14eba60560dd0
stevenzab <steven.zabandith@epitech.eu>
2026-03-26 10:30:47 +0100

    [ADD] unit tests for CatMatchDataAccess VoteCat method

---
+++ tests/CatMatch.Application.UnitTests/Services/CatMatch/CatMatchDataAccessTests.cs
@@ -0,0 +1,80 @@
+﻿using CatMatch.Application.Services.CatMatch;
+using CatMatch.Domain.Models;
+using CatMatch.Infrastructure.Common;
+using Moq;
+
+namespace CatMatch.Application.UnitTests.Services.CatMatch
+{
+    [TestClass]
+    public class CatMatchDataAccessTests
+    {
+        private Mock<IBaseRepository> mockBaseRepository;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            this.mockBaseRepository = new Mock<IBaseRepository>();
+        }
+
+        private CatMatchDataAccess CreateCatMatchDataAccess()
+        {
+            return new CatMatchDataAccess(this.mockBaseRepository.Object);
+        }
+
+        [TestMethod]
+        public async Task VoteCat_UpdatesVoteSuccessfully()
+        {
+            // Arrange
+            var cat = new Cat { Id = "1", Vote = 10 };
+            this.mockBaseRepository
+                .Setup(x => x.UpdateVoteAsync(cat.Id, cat.Vote))
+                .ReturnsAsync(true);
+
+            var catMatchDataAccess = this.CreateCatMatchDataAccess();
+
+            // Act
+            await catMatchDataAccess.VoteCat(cat);
+
+            // Assert
+            this.mockBaseRepository.Verify(x => x.UpdateVoteAsync(cat.Id, cat.Vote), Times.Once);
+        }
+
+        [TestMethod]
+        public async Task VoteCat_WithDifferentVoteValues()
+        {
+            // Arrange
+            var cat = new Cat { Id = "5", Vote = 100 };
+            this.mockBaseRepository
+                .Setup(x => x.UpdateVoteAsync(cat.Id, cat.Vote))
+                .ReturnsAsync(true);
+
+            var catMatchDataAccess = this.CreateCatMatchDataAccess();
+
+            // Act
+            await catMatchDataAccess.VoteCat(cat);
+
+            // Assert
+            this.mockBaseRepository.Verify(x => x.UpdateVoteAsync("5", 100), Times.Once);
+        }
+
+        [TestMethod]
+        public async Task VoteCat_CallsRepositoryUpdateVoteAsync()
+        {
+            // Arrange
+            var cat = new Cat { Id = "cat-123", Vote = 42 };
+            this.mockBaseRepository
+                .Setup(x => x.UpdateVoteAsync(cat.Id, cat.Vote))
+                .ReturnsAsync(true);
+
+            var catMatchDataAccess = this.CreateCatMatchDataAccess();
+
+            // Act
+            await catMatchDataAccess.VoteCat(cat);
+
+            // Assert
+            this.mockBaseRepository.Verify(
+                x => x.UpdateVoteAsync("cat-123", 42), 
+                Times.Once);
+        }
+    }
+}
